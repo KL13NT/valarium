e50e7b805470d6f1db0e311f15df6e74aabacd56
Nabil <nabil.tharwat@outlook.com>
2020-09-30 16:23:40 +0200

    fix: build breaking on config

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,27 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
-const ArticleHeader = ({ image, languageClass, title, author, date, path,imageCaption, tags }) =>
+const ArticleHeader = ({
+	image,
+	languageClass,
+	title,
+	author,
+	date,
+	path,
+	imageCaption,
+	tags
+}) => (
 	<>
-		<div className='o-article-header'>
-			<h1> { title } </h1>
+		<div className={`o-article-header ${languageClass}`}>
+			<h1> {title} </h1>
 			<div>
-				<p>By { author }</p>
-				<hr/>
+				<p>By {author}</p>
+				<hr />
 			</div>
-			<Img fluid={ image.childImageSharp.fluid }/>
-			<span className='c-image-caption'>{ imageCaption }</span>
+			<Img fluid={image.childImageSharp.fluid} />
+			<span className='c-image-caption'>{imageCaption}</span>
 		</div>
 	</>
+)
 
-const ArticleBody = ({ frontmatter, languageClass, html, fields }) =>
+const ArticleBody = ({ frontmatter, languageClass, html, fields }) => (
 	<article className='o-article-body'>
-		<ArticleHeader { ...frontmatter } { ...fields } languageClass={ languageClass }/>
-		<section className={ languageClass } dangerouslySetInnerHTML={ { __html: html } }/>
-		{
-		}
+		<ArticleHeader {...frontmatter} {...fields} languageClass={languageClass} />
+		<section
+			className={languageClass}
+			dangerouslySetInnerHTML={{ __html: html }}
+		/>
+		{}
 	</article>
-
+)
 
 ArticleHeader.propTypes = {
 	image: PropTypes.object.isRequired,
